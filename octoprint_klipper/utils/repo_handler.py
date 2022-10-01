96b8ceb21f157bbcbb7d16f92eb24d38f3842c54
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2022-10-01 02:54:40 +0200

    :construction: system commands rework

--- octoprint_klipper/utils/repo_handler.py
+++ octoprint_klipper/utils/repo_handler.py
@@ -3,19 +3,20 @@ import os
 import re
 
 from flask_babel import gettext
+import pkg_resources
 
-from extra import file_exist, run
-from logger import log_error, log_info
+import octoprint_klipper.utils.extra as extra
+import octoprint_klipper.utils.logger as logger
 
 
 def retrieve_git_tag(self, source_path):
     # TODO Drop python2.7 support like a hot potatoe
     cmd = "git -C " + source_path + " rev-list --tags --max-count=1"
-    commit_hash = run(self, cmd)
+    commit_hash = extra.execute_command(self, cmd)
     cmd = "git -C " + source_path + " describe --tags " + commit_hash
-    tag = run(self, cmd)
+    tag = extra.execute_command(self, cmd)
     cmd = "git -C " + source_path + " rev-list " + tag + "..HEAD --count"
-    count = run(self, cmd)
+    count = extra.execute_command(self, cmd)
     return "" + tag + "-" + count + ""
 
 
@@ -23,18 +24,31 @@ def retrieve_remote_git_tag(self, remote):
     # TODO Drop python2.7 support like a hot potatoe
     # Klipper Repo: https://github.com/Klipper3D/klipper.git
     # OctoKlipper Repo: https://github.com/thelastWallE/OctoprintKlipperPlugin.git
+
     if not self._connectivity_checker.online:
         return gettext("We are not online")
+
+    whole_git_version = extra.execute_command(self, "git --version").strip().split()
+    logger.log_info(self, "wholeline git version:" + whole_git_version[2], only_logging=True)
+    git_version = whole_git_version[2].split(".windows")[0]
+    logger.log_info(self, "parsed git version:" + git_version, only_logging=True)
+    if not pkg_resources.parse_version(git_version)>=pkg_resources.parse_version("2.18.0"):
+        return gettext("We are not over git version 2.18.0. Please upgrade to the latest version.")
+
     repo = self._settings.get(["configuration", "remote_host_git"]) if remote == "klipper" else remote
+    logger.log_info(self, "Retrieving remote tag for " + repo, only_logging=True)
     cmd = (
-        'git ls-remote --exit-code --refs --sort="version:refname" --tags '
+        "git ls-remote --exit-code --refs --sort=version:refname --tags "
         + repo
-        + ' "*.*.*" | tail --lines=1 | cut --delimiter="/" --fields=3'
+        + " | tail --lines=1 | cut --delimiter='/' --fields=3"
     )
-    # cmd = "git ls-remote --refs --sort='v:refname' --tags " + remote
+
+    # cmd = "git ls-remote --refs --sort=v:refname --tags " + remote
     # + " | tail --lines=1 | cut --delimiter='/' --fields=3"
-    output = run(self, cmd)
-    log_info(self, True, "retrieve_remote_git_tag Output: " + output)
+    output = extra.execute_command(self, cmd)
+    logger.log_info(
+        self, "retrieve_remote_git_tag Output: " + output, only_logging=True
+    )
     return output
 
 
@@ -43,7 +57,8 @@ def retrieve_remote_git_tag(self, remote):
 def retrieve_git_version(self, source_path):
     # Obtain version info from "git" program
     cmd = "git -C " + source_path + " describe --always --tags --long --dirty"
-    ver = run(self, cmd)
+    ver = extra.execute_command(self, cmd)
+    logger.log_info(self, "retrieve_git_version Output: " + ver, only_logging=True)
     tag_match = re.match(r"v\d+\.\d+\.\d+", ver)
     if tag_match is not None:
         return ver
@@ -56,7 +71,7 @@ def retrieve_git_version(self, source_path):
 
 def retrieve_last_git_tag(self, source_path):
     cmd = "git -C " + source_path + " describe --tags --abbrev=0"
-    return run(self, cmd)
+    return extra.execute_command(self, cmd)
 
 
 def get_software_version(self):
@@ -73,12 +88,17 @@ def get_software_version(self):
         version = retrieve_git_version(self, klipper_path)
     except Exception:
         vfile = os.path.join(klipper_path, "klippy", ".version")
-        if file_exist(self, vfile, PopUp=False):
-            try:
+
+        try:
+            if extra.file_exist(self, vfile, PopUp=False):
                 version = vfile.read_text().strip()
-            except Exception:
-                log_error(self, False, gettext("Unable to extract version from file"))
-                version = "?"
+        except Exception:
+            logger.log_error(
+                self,
+                gettext("Unable to extract version from file"),
+                only_logging=True,
+            )
+            return "?"
     return version
 
 
@@ -90,9 +110,9 @@ def update_klipper_host(self, tag):
         )
     )
     cmd = "git -C " + klipper_path + " fetch --all --tags"
-    run(self, cmd)
+    extra.run(self, cmd)
     cmd = "git -C " + klipper_path + " pull"
-    run(self, cmd)
+    extra.run(self, cmd)
     cmd = "git -C " + klipper_path + " checkout " + tag
 
-    return run(self, cmd)
+    return extra.run(self, cmd)
