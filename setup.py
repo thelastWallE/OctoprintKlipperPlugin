2e4fe3ba005550de4fe0e2f07ebb96109764a1c5
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2022-10-02 11:15:07 +0200

    :rocket: increase versionnumber and update pre-commit

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ plugin_package = "octoprint_klipper"
 
 plugin_name = "OctoKlipper"
 
-plugin_version = "0.3.9.4"
+plugin_version = "0.4rc"
 
 plugin_description = """A plugin for OctoPrint to configure,control and monitor the Klipper 3D printer software."""
 
