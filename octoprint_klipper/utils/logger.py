d356c5764129a63cb6aa60eef73c87eda8c091e9
thelastWallE <12502210+thelastWallE@users.noreply.github.com>
2023-01-14 15:12:00 +0100

    📝 docstrings for logger functions

--- octoprint_klipper/utils/logger.py
+++ octoprint_klipper/utils/logger.py
@@ -8,6 +8,13 @@ else:
 
 
 def log_info(self, message, only_logging=False):
+    """Logs a message into the logfile, to the Klipper-Logmessages if debug_logging is enabled and to the console per default.
+
+    :param message: The text to log
+    :type message: str
+    :param only_logging: don't log to the console if true, defaults to False
+    :type only_logging: bool, optional
+    """
     self._octoklipper_logger.info(message)
 
     if self._settings.get_boolean(["configuration", "debug_logging"]):
@@ -19,6 +26,13 @@ def log_info(self, message, only_logging=False):
 
 
 def log_debug(self, message, only_logging=False):
+    """Logs a message into the logfile, to the Klipper-Logmessages if debug_logging is enabled and to the console per default.
+
+    :param message: The text to log
+    :type message: str
+    :param only_logging: don't log to the console if true, defaults to False
+    :type only_logging: bool, optional
+    """
     self._octoklipper_logger.debug(message)
     self._logger.info(message)
 
@@ -31,6 +45,13 @@ def log_debug(self, message, only_logging=False):
 
 
 def log_error(self, error, only_logging=False):
+    """Logs a message into the logfile, to the Klipper-Logmessages if debug_logging is enabled and to the console per default.
+
+    :param message: The text to log
+    :type message: str
+    :param only_logging: don't log to the console if true, defaults to False
+    :type only_logging: bool, optional
+    """
     self._octoklipper_logger.error(error)
     self._logger.error(error)
 
